e27a8e9b40e939136171e273d1c0ed22f04b3d59
ajovanovic <aleksandarjovanovic21@gmail.com>
2016-09-30 11:47:25 +0200

    parse json to collection of cards

--- src/main/java/org/leanpoker/player/PlayerObject.java
+++ src/main/java/org/leanpoker/player/PlayerObject.java
@@ -9,7 +9,7 @@
 // it only in accordance with the terms of the license agreement
 // you entered into with Betware.
 //==============================================================================
-package main.java.org.leanpoker.player;
+package org.leanpoker.player;
 
 import java.util.List;
 
