58c91e92671e78dc9adc1a72a50452d24bf16cc3
ajovanovic <aleksandarjovanovic21@gmail.com>
2016-09-30 10:55:33 +0200

    call on pair in hand

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "good ol' betware";
 
     public static int betRequest(JsonElement request) {
         JsonObject obj = request.getAsJsonObject();
@@ -16,8 +16,24 @@ public class Player {
         JsonArray igraci = obj.getAsJsonArray("players");
         JsonObject mi = igraci.get(ind).getAsJsonObject();
         JsonArray nase_karte = mi.get("hole_cards").getAsJsonArray();
-        if (nase_karte.get(0).getAsJsonObject().get("rank").getAsString() == nase_karte.get(0).getAsJsonObject().get("rank").getAsString()){
-            return obj.get("current_buy_in").getAsInt() - mi.get("bet").getAsInt() + obj.get("minimum_raise").getAsInt();
+        JsonArray karte_na_stolu = obj.get("community_cards").getAsJsonArray();
+        int br_istih_sa_1 = 0;
+        int br_istih_sa_2 = 0;
+
+        for (JsonElement jsonElement : karte_na_stolu) {
+            if(jsonElement.getAsJsonObject().get("rank").getAsString() == nase_karte.get(0).getAsJsonObject().get("rank").getAsString()){
+                br_istih_sa_1++;
+            }
+            if(jsonElement.getAsJsonObject().get("rank").getAsString() == nase_karte.get(1).getAsJsonObject().get("rank").getAsString()){
+                br_istih_sa_2++;
+            }
+        }
+
+        if (nase_karte.get(0).getAsJsonObject().get("rank").getAsString() == nase_karte.get(1).getAsJsonObject().get("rank").getAsString()){
+            if (br_istih_sa_1 != 0){
+                return obj.get("current_buy_in").getAsInt() - mi.get("bet").getAsInt() + obj.get("minimum_raise").getAsInt()  + obj.get("minimum_raise").getAsInt();
+            }
+            return obj.get("current_buy_in").getAsInt() - mi.get("bet").getAsInt();
         }
         return 0;
     }
