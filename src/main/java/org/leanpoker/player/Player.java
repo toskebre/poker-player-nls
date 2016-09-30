8954f177138a43cf64264a05ce26637a2d84e401
ajovanovic <aleksandarjovanovic21@gmail.com>
2016-09-30 10:40:36 +0200

    call on pair in hand

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,6 +11,14 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        JsonObject obj = request.getAsJsonObject();
+        int ind = obj.get("in_action").getAsInt();
+        JsonArray igraci = obj.getAsJsonArray("players");
+        JsonObject mi = igraci.get(ind).getAsJsonObject();
+        JsonArray nase_karte = mi.get("hole_cards").getAsJsonArray();
+        if (nase_karte.get(0).getAsJsonObject().get("rank").getAsString() == nase_karte.get(0).getAsJsonObject().get("rank").getAsString()){
+            return obj.get("current_buy_in").getAsInt() - mi.get("bet").getAsInt() + obj.get("minimum_raise").getAsInt();
+        }
         return 0;
     }
 
