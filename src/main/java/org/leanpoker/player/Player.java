0800277277d82436b8e0d6fcfc680e10e3699538
ajovanovic <aleksandarjovanovic21@gmail.com>
2016-09-30 14:55:24 +0200

    zbir

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,6 +3,7 @@ package org.leanpoker.player;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.leanpoker.combinations.AStoIgrati;
 
 import java.util.Map;
 
@@ -41,6 +42,9 @@ public class Player {
                     break;
             }
             zbir += Integer.parseInt(r);
+
+            karta = new Card(Card.CardSign.valueOf(suit), Player.getNumber(r));
+            kolekcija.addCard(karta);
         }
 
 
@@ -50,8 +54,6 @@ public class Player {
         for (JsonElement jsonElement : karte_na_stolu) {
             String suit = jsonElement.getAsJsonObject().get("suit").getAsString().toUpperCase();
             String rank = jsonElement.getAsJsonObject().get("rank").getAsString().toUpperCase();
-            //karta = new Card(Card.CardSign.valueOf(suit), Card.CardNumber.valueOf(rank));
-            //kolekcija.addCard(karta);
 
             if(jsonElement.getAsJsonObject().get("rank").getAsString().equals(nase_karte.get(0).getAsJsonObject().get("rank").getAsString())){
                 br_istih_sa_1++;
@@ -59,6 +61,36 @@ public class Player {
             if(jsonElement.getAsJsonObject().get("rank").getAsString().equals(nase_karte.get(1).getAsJsonObject().get("rank").getAsString())){
                 br_istih_sa_2++;
             }
+
+            String r = rank;
+            switch (rank){
+                case "A":
+                    r = "14";
+                    break;
+                case "J":
+                    r = "11";
+                    break;
+                case "Q":
+                    r = "12";
+                    break;
+                case "K":
+                    r = "13";
+                    break;
+            }
+            karta = new Card(Card.CardSign.valueOf(suit), Player.getNumber(r));
+            kolekcija.addCard(karta);
+        }
+
+        AStoIgrati staIgrati = new AStoIgrati();
+        AStoIgrati.Action action = staIgrati.staOdigrati(kolekcija);
+
+        switch (action){
+            case ALL_IN:
+                return obj.get("current_buy_in").getAsInt() - mi.get("bet").getAsInt() + obj.get("minimum_raise").getAsInt();
+            case FOLLOW:
+                return obj.get("current_buy_in").getAsInt() - mi.get("bet").getAsInt();
+            case RAISE:
+                return obj.get("current_buy_in").getAsInt() - mi.get("bet").getAsInt() + obj.get("minimum_raise").getAsInt();
         }
 
         if (nase_karte.get(0).getAsJsonObject().get("rank").getAsString().equals(nase_karte.get(1).getAsJsonObject().get("rank").getAsString())){
@@ -81,5 +113,37 @@ public class Player {
     public static void showdown(JsonElement game) {
     }
 
+    public static Card.CardNumber getNumber(String number){
+        switch (number){
+            case "1":
+                return Card.CardNumber.ONE;
+            case "2":
+                return Card.CardNumber.TWO;
+            case "3":
+                return Card.CardNumber.THREE;
+            case "4":
+                return Card.CardNumber.FOUR;
+            case "5":
+                return Card.CardNumber.FIVE;
+            case "6":
+                return Card.CardNumber.SIX;
+            case "7":
+                return Card.CardNumber.SEVEN;
+            case "8":
+                return Card.CardNumber.EIGHT;
+            case "9":
+                return Card.CardNumber.NINE;
+            case "10":
+                return Card.CardNumber.TEN;
+            case "11":
+                return Card.CardNumber.J;
+            case "12":
+                return Card.CardNumber.Q;
+            case "13":
+                return Card.CardNumber.K;
+            case "14":
+                return Card.CardNumber.A;
+        }
+    }
 
 }
